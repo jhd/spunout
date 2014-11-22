6f2e4b7727b8e12fd6ecff4950dc749f12cd916b
jhd <jhennes@tcd.ie>
2014-11-22 13:14:17 +0000

    Added databse fetch functions

--- databaseConnector.py
+++ databaseConnector.py
@@ -13,3 +13,30 @@ def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
+def query_db(query, args=(), one=False):
+     cur = get_db().execute(query, args)
+     rv = cur.fetchall()
+     cur.close()
+     return (rv[0] if rv else None) if one else rv
+
+def get_user(user_id):
+    user = query_db('select * from users where user_id = ?', [user_id], one=True)
+    if user is None:
+        return None
+    else:
+        return user
+
+def get_responce(reponce_id):
+    responce = query_db('select * from responces where responce_id = ?', [responce_id], one=True)
+    if responce is None:
+        return None
+    else:
+        return responce
+
+def get_responces(user_id):
+    responces = query_db('select * from responces where user_id = ?', [user_id], One = False)
+    if responce is None:
+        return None
+    else:
+        return responce
